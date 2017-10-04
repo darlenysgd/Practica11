6c42921b4f96cfb2bc54150ab3713017f4914452
darlenys <darlenysgz@gmail.com>
2017-10-04 11:15:43 -0400

    Initial commit

---
+++ src/main/java/quiz3/Quiz3Application.java
@@ -0,0 +1,12 @@
+package quiz3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quiz3Application.class, args);
+	}
+}
