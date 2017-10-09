852b2d5eedee1f8e534c3edcbc1194557a07f161
darlenys <darlenysgz@gmail.com>
2017-10-08 21:44:43 -0400

    Home y base de datos funcionando!

---
+++ src/main/java/quiz3/Configuraciones/MvcConfig.java
@@ -0,0 +1,20 @@
+package quiz3.Configuraciones;
+
+/**
+ * Created by darle on 10/8/2017.
+ */
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/Home").setViewName("index");
+
+    }
+
+}
--- src/main/java/quiz3/Controladores/ClienteFormController.java
+++ src/main/java/quiz3/Controladores/ClienteFormController.java
@@ -4,10 +4,17 @@ package quiz3.Controladores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import quiz3.Entidades.Cliente;
 import quiz3.Servicios.ClienteServices;
 import quiz3.repositorio.ClienteRepository;
+
+import java.io.ByteArrayInputStream;
+import java.nio.file.Files;
 
 
 /**
@@ -17,13 +24,25 @@ import quiz3.repositorio.ClienteRepository;
 @RequestMapping("/clientes")
 public class ClienteFormController {
 
+
+
     @Autowired
     ClienteServices clienteServices;
 
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public String RegistroCliente(Model model) {
+       @RequestMapping(value = "", method = RequestMethod.GET)
+       public String RegistroCliente() {
+
+           return "/RegistroCliente";
+        }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public String GuardarForm(@ModelAttribute Cliente cliente)   {
+
+        clienteServices.crearCliente(cliente);
+
         return "/RegistroCliente";
-    }
+        }
+
 
 }
