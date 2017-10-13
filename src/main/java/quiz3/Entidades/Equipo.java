1955d0cfdea287231d35a4f92e985310c1a9e782
darlenys <darlenysgz@gmail.com>
2017-10-13 10:49:19 -0400

    Registro de equipos.

--- src/main/java/quiz3/Entidades/Equipo.java
+++ src/main/java/quiz3/Entidades/Equipo.java
@@ -3,6 +3,7 @@ package quiz3.Entidades;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.List;
 
 /**
  * Created by darle on 10/12/2017.
@@ -14,20 +15,13 @@ public class Equipo {
     @GeneratedValue
     private long id;
     private String nombre;
-    private String marca;
-    private String modelo;
-    private long valor;
-    private long precioAlquiler;
+    private List<SubEquipos> subEquiposList;
 
     public Equipo() {
     }
 
-    public Equipo(String nombre, String marca, String modelo, long valor, long precioAlquiler) {
+    public Equipo(String nombre) {
         this.nombre = nombre;
-        this.marca = marca;
-        this.modelo = modelo;
-        this.valor = valor;
-        this.precioAlquiler = precioAlquiler;
     }
 
     public long getId() {
@@ -46,35 +40,11 @@ public class Equipo {
         this.nombre = nombre;
     }
 
-    public String getMarca() {
-        return marca;
+    public List<SubEquipos> getSubEquiposList() {
+        return subEquiposList;
     }
 
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public long getValor() {
-        return valor;
-    }
-
-    public void setValor(long valor) {
-        this.valor = valor;
-    }
-
-    public long getPrecioAlquiler() {
-        return precioAlquiler;
-    }
-
-    public void setPrecioAlquiler(long precioAlquiler) {
-        this.precioAlquiler = precioAlquiler;
+    public void setSubEquiposList(List<SubEquipos> subEquiposList) {
+        this.subEquiposList = subEquiposList;
     }
 }
